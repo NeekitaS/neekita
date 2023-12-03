bddff00bc6e19f295838c177c85925a1e402ac3a
Neekita Sharma <91779793+NeekitaS@users.noreply.github.com>
2023-12-03 16:44:05 +0530

    Update firstDsa.cpp

--- firstDsa.cpp
+++ firstDsa.cpp
@@ -1,1 +1,16 @@
-// fi
+// first program
+#include<iostream>
+using namespace std;
+#inlcude<string.h>
+int main()
+{
+  char l;
+	cin>>l;
+	if(isupper(l))
+	cout<<"1";
+	else if (islower(l))
+	cout<<"0";
+	else 
+	cout<<"-1";
+	return 0;
+}
